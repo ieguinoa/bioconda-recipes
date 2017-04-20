4ca592f59b2371ee5881685aad7ff08f9b7dd3fa
Ignacio Eguinoa <igegu@psb.ugent.be>
2017-04-20 13:51:26 +0200

    add cctop recipe

---
+++ recipes/cctop/run_test.sh
@@ -0,0 +1,6 @@
+#source /home/igegu/miniconda2/bin/activate cctop
+#module load cctop
+bowtie-build -r -f phiX.fasta phiX_index
+bowtie_bin=$(which bowtie)
+bowtie_dir=$(dirname $bowtie_bin)
+CCTop.py --input test_input.fasta --index phiX_index --bowtie $bowtie_dir
